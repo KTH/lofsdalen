9327b55a3c5fac9f0d9beb8cf5631b9707476a26
Patric Jansson <paddy@kth.se>
2020-10-01 16:40:24 +0200

    No test added as test.

---
+++ tests/unit-tests/test-app.js
@@ -0,0 +1,11 @@
+/* eslint-env mocha */
+"use strict";
+
+// Testing libraries
+const expect = require("chai").expect;
+
+describe("Test", function () {
+  it("No tests implemented yet.", function () {
+    expect(true).to.be.true;
+  });
+});
